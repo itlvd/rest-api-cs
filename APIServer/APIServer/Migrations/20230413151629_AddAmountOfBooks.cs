28cd2b6aa7183390cd6f4f043c7fb818f42e0d12
Le Van Dong <itlvd@outlook.com>
2023-04-13 22:28:20 +0700

    build: add new properties of book

---
+++ APIServer/APIServer/Migrations/20230413151629_AddAmountOfBooks.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAmountOfBooks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Amount",
+                table: "Book",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Book");
+        }
+    }
+}
